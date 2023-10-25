e13070e7a9b17ac3262f154db356db036a3edeb2
Bro-One <148443656+Bro-One@users.noreply.github.com>
2023-10-25 18:18:37 +0900

    Update single.py

--- single.py
+++ single.py
@@ -1,5 +1,5 @@
 from pyOptoSigma import *
-from getkey import getkey
+from getkey import getkey, keys
 
 
 # This file is for controlling 'single' motorized stage
@@ -8,39 +8,40 @@ from getkey import getkey
 # define functions
 def prt_f():
     print("== complete! ==\n")
-
-
-def prt_l(d):
-    d = stages.get_position()
-    print("location = {}".format(d))
-
+def prt_l():
+    print("location = {}".format(stages.get_position()))
+# print and get a current position
 
 # define step size
 step = 1000
 # 1000um = 1mm
 
 # define maximum length
-max_len = 85000
-
+max_len_1 = 85000
+max_len_2 = 100000
 # set connection
 stages = Session(Controllers.SHOT_702)
 stages.append_stage(Stages.OSMS20_85)
+stages.append_stage(Stages.OSMS26_100)
 stages.connect()
 
+# initialize
+print("****** initialize the location ******")
+stages.initialize()
+
 # define location
-location = 0
+location = stages.get_position()
 
 print("****** current location ******")
-prt_l(location)
 
-# print(type(location))
 msg = '''
 ****** Ready for keyboard input ******
 
  press the arrow keys in the direction you want to move. 
  
  r : set current position as new origin.
- i : come back to the origin. (initialize)
+ i : come back to the absolute origin. (initialize)
+ c : show the current position
  
  unit = micro-meter (um)
  if you want to exit, press 'q' or ctrl + c
@@ -50,26 +51,56 @@ print(msg)
 while 1:
     key = getkey()
     print("key ={}".format(key))
+    # control the stage 1 (vertical)
     if key == keys.UP:
-        stages.move(amount=step, wait_for_finish=True)  # 1000um = 1mm
-        prt_l(location)
-        prt_f()
+        if stages.get_position()[0] + step > max_len_1:
+            print("out of range!")
+            continue
+        else:
+            stages.move(stage=1, amount=step, wait_for_finish=True)  # 1000um = 1mm
+            prt_l()
+            prt_f()
 
     elif key == keys.DOWN:
-        stages.move(amount=-step, wait_for_finish=True)
-        prt_l(location)
-        prt_f()
+        # print("location - step = ", stages.get_position()[0] - step)
+        if stages.get_position()[0] - step < 0:
+            print("out of range!")
+            continue
+        else:
+            stages.move(stage=1, amount=-step, wait_for_finish=True)
+            prt_l()
+            prt_f()
+    # control the stage 2 (horizontal)
+    elif key == keys.RIGHT:
+        if stages.get_position()[1] + step > max_len_2:
+            print("out of range!")
+            continue
+        else:
+            stages.move(stage=2, amount=step, wait_for_finish=True)
+            prt_l()
+            prt_f()
+    elif key == keys.LEFT:
+        if stages.get_position()[1] - step < 0:
+            print("out of range!")
+            continue
+        else:
+            stages.move(stage=2, amount=-step, wait_for_finish=True)
+            prt_l()
+            prt_f()
+    elif key == 'c':
+        print("== current position ==")
+        prt_l()
 
     elif key == 'r':
         print("== set current position as new origin ==")
-        stages.reset(mechanical=True)
-        prt_l(location)
+        stages.reset()
+        prt_l()
         prt_f()
 
     elif key == 'i':
         print("== initialize the location ==")
         stages.initialize()
-        prt_l(location)
+        prt_l()
         prt_f()
 
     elif key == 'q':
